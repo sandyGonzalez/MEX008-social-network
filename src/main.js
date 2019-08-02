05fe0844d122150309c290a1f2301dcb1445661a
TaniaTenorio <taniatenorio@hotmail.com>
2019-08-02 17:29:58 -0500

    se editó el readme, con inamgenes y links de prototipos

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+const logIn = document.getElementById("login-btn");
+const register = () =>  
+{
+    // console.log("diste un click");
+    let email = document.getElementById("login-email").value;
+    let password = document.getElementById("login-password").value;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+      }); 
+};
+logIn.addEventListener("click", register);
